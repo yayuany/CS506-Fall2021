4f189370ef3288b70f7d02804f461a55e01166bb
Yayuan Yuan <yayuany@bu.edu>
2021-10-03 09:42:41 -0400

    read and sim

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -2,5 +2,16 @@ def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
-    """
+    """ 
+
+    l=[]
+
+    with open(csv_file_path, 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        l.append([x for x in line.split(",") if x != '\n"])
+
+
+    return l 
     
+
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -6,17 +6,44 @@ def euclidean_dist(x, y):
 
 
 def manhattan_dist(x, y):
+    sum = 0
+    for i in range(len(x)):
+        sunm += abs(x[i]-y[i])
 
-
-
-    
+    return sum   
 
 def jaccard_dist(x, y):
-    
-
+    intersection = set(x and y)
+    card_i = len(intersection)
+    union = set( x or y)
+    card_u = len(union)
+    if card_i == 0:
+        return 0
+    elif card_u == 0:
+        return 'undefined'
+    else:
+        return 1 - ((float)card_i /card_u)
 
 
 def cosine_sim(x, y):
+    dot = 0
+    x_len = 0
+    y_len = 0
     
+    for i in x:
+        dot += x[i] * y[i]
+        x_len += x[i]**2
+        y_len += y[i]**2
+        
+    x_len = xlen**(1/2)
+    y_len = ylen**(1/2)
+    
+    c_product = x_len * y_len
+ 
+    if c_product == 0:
+        return 'undefined'
+    else:
+        return dot / c_product   
+ 
 
 # Feel free to add more
